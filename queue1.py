415fc63e1edbb45c8cfb6939d3113c39477f8c9d
renwish <eugenebasinio@gmail.com>
2022-12-17 20:05:38 +0800

    added rich and missing modules

--- queue1.py
+++ queue1.py
@@ -5,6 +5,15 @@ import argparse
 from queue import LifoQueue, PriorityQueue, Queue
 from random import randint
 from time import sleep
+from random import choice, randint
+
+from itertools import zip_longest
+
+from rich.align import Align
+from rich.columns import Columns
+from rich.console import Group
+from rich.live import Live
+from rich.panel import Panel
 
 QUEUE_TYPES = {
     "fifo": Queue,
