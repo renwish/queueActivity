673e6e5c275191c7cbd1907b6dc6e4a6e15e1b88
renwish <eugenebasinio@gmail.com>
2022-12-17 19:13:27 +0800

    1st commit

---
+++ queue1.py
@@ -0,0 +1,10 @@
+# thread_safe_queues.py
+
+import argparse
+from queue import LifoQueue, PriorityQueue, Queue
+
+QUEUE_TYPES = {
+    "fifo": Queue,
+    "lifo": LifoQueue,
+    "heap": PriorityQueue
+}
